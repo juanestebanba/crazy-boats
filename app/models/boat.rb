db9cd2a67846770a163807da1f4804cbc931b56a
Jorge Alejandro Avelar Gonzalez <avelargonzalez@gmail.com>
2022-05-19 20:23:01 -0500

    fixing validations

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,5 +1,8 @@
 class Boat < ApplicationRecord
   has_many :bookings, dependent: :destroy
   belongs_to :user
-  validate :name, :category, :capacity, :price, presence: true
+  validates :name, presence: true
+  validates :category, presence: true
+  validates :capacity, presence: true
+  validates :price, presence: true
 end
