ec4ddff937ff4dfa39f3f2045c9cb035d1922c67
Jorge Alejandro Avelar Gonzalez <avelargonzalez@gmail.com>
2022-05-21 09:46:57 -0500

    categories-controller unfinished

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,13 @@
 class CategoriesController < ApplicationController
-  def controller
+  def fishingboats
+    @boats = Boat.where(category: "fishing_boat")
+  end
+
+  def yachts
+    @boats = Boat.where(category: "yachts")
+  end
+
+  def sportsboat
+    @boats = Boat.where(category: "sports_boat")
   end
 end
