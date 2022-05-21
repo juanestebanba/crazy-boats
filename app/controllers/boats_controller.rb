545ce46a01d58f360c165c916c8ddee145b1f231
Jorge Alejandro Avelar Gonzalez <avelargonzalez@gmail.com>
2022-05-21 16:44:04 -0500

    adding a boat

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,5 +1,5 @@
 class BoatsController < ApplicationController
-  skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @boats = Boat.all
   end
@@ -8,33 +8,27 @@ class BoatsController < ApplicationController
     set_boat
   end
 
-  # def new
-  #   find_user
-  #   @boat = Boat.new
-  # end
+  def new
+    @boat = Boat.new
+  end
 
-  # def create
-  #   find_user
-  #   @boat = Boat.new(boat_params)
-  #   @boat.user = @user
-  #   if @boat.save
-  #     redirect_to boat_path(@boat)
-  #   else
-  #     render :new
-  #   end
-  # end
+  def create
+    @boat = Boat.new(boat_params)
+    @boat.user = current_user
+    if @boat.save!
+      redirect_to boat_path(@boat)
+    else
+      render :new
+    end
+  end
 
   private
-
-  def set_user
-    @user = User.find(params[:id])
-  end
 
   def set_boat
     @boat = Boat.find(params[:id])
   end
 
   def boat_params
-    params.require(:boat).permit(:name, :cateogry, :capacity, :price, :user_id)
+    params.require(:boat).permit(:name, :category, :capacity, :price, :user_id)
   end
 end
